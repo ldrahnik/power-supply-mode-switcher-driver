547e45c97ade3b42c32db49bc1a21c1b9f729550
Lukáš Drahník <ldrahnik@gmail.com>
2023-02-02 18:38:46 +0100

    Removed unnecessary sudo from scripts

--- conf/ac_mode.sh
+++ conf/ac_mode.sh
@@ -3,4 +3,4 @@
 # set normal display brightness
 FILE="/sys/class/backlight/intel_backlight/brightness";
 echo "Display brightness change to"
-echo "405" | sudo tee $FILE
+echo "405" | tee $FILE
--- conf/bat_mode.sh
+++ conf/bat_mode.sh
@@ -3,11 +3,11 @@
 # set lower display brightness
 FILE="/sys/class/backlight/intel_backlight/brightness";
 echo "Display brightness change to"
-echo "92" | sudo tee $FILE
+echo "92" | tee $FILE
 
 # set backlight to lowest level when is backlight enabled and level is set up more intensive
 CURRENT_KEYBOARD_BACKLIGHT="$(cat /sys/class/leds/asus::kbd_backlight/brightness)"
 if [[ "$CURRENT_KEYBOARD_BACKLIGHT" > 1 ]]; 
 then
-    echo 1 | sudo tee /sys/class/leds/asus::kbd_backlight/brightness
+    echo 1 | tee /sys/class/leds/asus::kbd_backlight/brightness
 fi

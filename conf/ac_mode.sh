cc4d6704a3828d88dd76fd8d40fa597cab7bbe43
Lukáš Drahník <ldrahnik@gmail.com>
2023-03-05 23:35:15 +0100

    Added switching profiles of Elementary OS Power management tool

--- conf/ac_mode.sh
+++ conf/ac_mode.sh
@@ -8,3 +8,6 @@ echo "400" | tee $FILE
 # intel_pstate
 FILE="/sys/devices/system/cpu/intel_pstate"
 echo "0" | tee $FILE/no_turbo
+
+# elementary os Power management tool (https://gitlab.freedesktop.org/hadess/power-profiles-daemon)
+powerprofilesctl set performance
--- conf/bat_mode.sh
+++ conf/bat_mode.sh
@@ -15,3 +15,6 @@ fi
 # intel_pstate
 FILE="/sys/devices/system/cpu/intel_pstate"
 echo "1" | tee $FILE/no_turbo
+
+# elementary os Power management tool (https://gitlab.freedesktop.org/hadess/power-profiles-daemon)
+powerprofilesctl set power-saver
